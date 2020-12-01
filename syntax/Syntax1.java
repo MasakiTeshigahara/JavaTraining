0dc56e33ee8ea3fa90dbaa0d192b71b408f61c44
unknown <masaki.teshi@gmail.com>
2020-12-01 22:44:06 +0900

    コードのコメント追加

--- syntax/Syntax1.java
+++ syntax/Syntax1.java
@@ -2,10 +2,12 @@ package syntax;
 
 public class Syntax1 {
 	public static void main(String[] args) throws Exception{
+		//引数の長さを取得し設定数を表示する
 		System.out.println("引数は" + args.length + "個設定されています。");
+		//引数の長さの回数分だけ以下の処理を繰り返す
 		for(int idx = 0 ; idx < args.length; idx ++) {
+			//argsのStringリストに格納されているidx番ごと出力する
 			System.out.println((idx + 1) + "番目の引数は「" + args[idx] + "」です。");
-
 		}
 	}
 
